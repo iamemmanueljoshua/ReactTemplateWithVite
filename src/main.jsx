c820913c7a5f9461ca7948f2f6b08cf0f348a1af
Emmanuel Joshua <iamemmanueljoshua@gmail.com>
2023-05-19 10:25:19 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './assets/css/style.css'
+import 'material-icons/iconfont/material-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bulma/css/bulma.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './assets/css/style.css'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    <Footer />
+  </React.StrictMode>
 )
